5c040d7dc4a859308ba28c5b2d08b423521e2520
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-08-29 15:01:08 -0300

    refactored player state machine

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,109 +1,156 @@
 extends KinematicBody2D
 
 const PARTICLES = preload("res://scenes/RunParticles.tscn")
+
 onready var animation_player = get_node("AnimationPlayer")
 onready var sprite = get_node("Sprite")
-var velocity
-var last_direction
-var is_attacking
+
 export(int) var speed = 60
 
-enum { 
-	IDLE_FRONT, 
-	IDLE_SIDE_LEFT, 
-	IDLE_SIDE_RIGHT, 
-	IDLE_BACK, 
-	RUN_FRONT, 
-	RUN_SIDE_LEFT, 
-	RUN_SIDE_RIGHT, 
-	RUN_BACK, 
+var velocity = 0
+
+var is_running = false
+var is_attacking = false
+var current_state = ANIMATIONS[IDLE_FRONT]
+var last_direction
+
+enum {
+	IDLE_FRONT,
+	IDLE_SIDE_LEFT,
+	IDLE_SIDE_RIGHT,
+	IDLE_BACK,
+	RUN_FRONT,
+	RUN_SIDE_LEFT,
+	RUN_SIDE_RIGHT,
+	RUN_BACK,
 	RUN_DIAGONAL_Q1,
 	RUN_DIAGONAL_Q2,
 	RUN_DIAGONAL_Q3,
 	RUN_DIAGONAL_Q4,
-	ATTACK_FRONT, 
-	ATTACK_SIDE_LEFT, 
-	ATTACK_SIDE_RIGHT, 
-	ATTACK_BACK, 
+	ATTACK_FRONT,
+	ATTACK_SIDE_LEFT,
+	ATTACK_SIDE_RIGHT,
+	ATTACK_BACK,
 }
 
-var directions = {
-	Vector2.ZERO: IDLE_FRONT,
-	Vector2.UP: RUN_BACK,
-	Vector2.DOWN: RUN_FRONT,
-	Vector2.RIGHT: RUN_SIDE_RIGHT,
-	Vector2.LEFT: RUN_SIDE_LEFT,
-	Vector2(-1,-1): RUN_DIAGONAL_Q2,
-	Vector2(1,-1): RUN_DIAGONAL_Q1,
-	Vector2(-1,1): RUN_DIAGONAL_Q3,
-	Vector2(1,1): RUN_DIAGONAL_Q4,
-}
-var animation = {
+const ANIMATIONS = {
 	IDLE_FRONT: {
-		"name": "idle_front",
+		"anim_name": "idle_front",
 		"flip": false,
 	},
 	IDLE_SIDE_LEFT: {
-		"name": "idle_side",
+		"anim_name": "idle_side",
 		"flip": true,
-	}, 
+	},
 	IDLE_SIDE_RIGHT: {
-		"name": "idle_side",
+		"anim_name": "idle_side",
 		"flip": false,
 	},
 	IDLE_BACK: {
-		"name": "idle_back",
+		"anim_name": "idle_back",
 		"flip": false,
 	},
 	RUN_FRONT: {
-		"name": "run_front",
+		"anim_name": "run_front",
 		"flip": false,
 	},
 	RUN_SIDE_LEFT: {
-		"name": "run_side",
+		"anim_name": "run_side",
 		"flip": true,
-	}, 
+	},
 	RUN_SIDE_RIGHT: {
-		"name": "run_side",
+		"anim_name": "run_side",
 		"flip": false,
-	}, 
+	},
 	RUN_BACK: {
-		"name": "run_back",
+		"anim_name": "run_back",
 		"flip": false,
-	},  
+	},
 	RUN_DIAGONAL_Q1: {
-		"name": "run_back",
+		"anim_name": "run_back",
 		"flip": false,
-	}, 
+	},
 	RUN_DIAGONAL_Q2: {
-		"name": "run_back",
+		"anim_name": "run_back",
 		"flip": false,
-	}, 
+	},
 	RUN_DIAGONAL_Q3: {
-		"name": "run_front",
+		"anim_name": "run_front",
 		"flip": false,
 	},
 	RUN_DIAGONAL_Q4: {
-		"name": "run_front",
+		"anim_name": "run_front",
 		"flip": false,
 	},
 	ATTACK_FRONT: {
-		"name": "attack_front",
+		"anim_name": "attack_front",
 		"flip": false,
-	}, 
+	},
 	ATTACK_SIDE_LEFT: {
-		"name": "attack_side",
+		"anim_name": "attack_side",
 		"flip": true,
-	}, 
+	},
 	ATTACK_SIDE_RIGHT: {
-		"name": "attack_side",
+		"anim_name": "attack_side",
 		"flip": false,
 	},
 	ATTACK_BACK: {
-		"name": "attack_back",
+		"anim_name": "attack_back",
 		"flip": false,
 	},
 }
+
+const DIRECTIONS = {
+	Vector2.ZERO: {
+		"q0": ANIMATIONS[IDLE_FRONT],
+		"q1": ANIMATIONS[RUN_FRONT],
+		"q2": ANIMATIONS[ATTACK_FRONT],
+	},
+	Vector2.LEFT: {
+		"q0": ANIMATIONS[IDLE_SIDE_LEFT],
+		"q1": ANIMATIONS[RUN_SIDE_LEFT],
+		"q2": ANIMATIONS[ATTACK_SIDE_LEFT],
+	},
+	Vector2.RIGHT: {
+		"q0": ANIMATIONS[IDLE_SIDE_RIGHT],
+		"q1": ANIMATIONS[RUN_SIDE_RIGHT],
+		"q2": ANIMATIONS[ATTACK_SIDE_RIGHT],
+	},
+	Vector2.UP: {
+		"q0": ANIMATIONS[IDLE_BACK],
+		"q1": ANIMATIONS[RUN_BACK],
+		"q2": ANIMATIONS[ATTACK_BACK],
+	},
+	Vector2.DOWN: {
+		"q0": ANIMATIONS[IDLE_FRONT],
+		"q1": ANIMATIONS[RUN_FRONT],
+		"q2": ANIMATIONS[ATTACK_FRONT],
+	},
+	Vector2(1, -1): {
+		"q0": ANIMATIONS[IDLE_BACK],
+		"q1": ANIMATIONS[RUN_DIAGONAL_Q1],
+		"q2": ANIMATIONS[ATTACK_BACK],
+	},
+	Vector2(-1, -1): {
+		"q0": ANIMATIONS[IDLE_BACK],
+		"q1": ANIMATIONS[RUN_DIAGONAL_Q2],
+		"q2": ANIMATIONS[ATTACK_BACK],
+	},
+	Vector2(-1, 1): {
+		"q0": ANIMATIONS[IDLE_FRONT],
+		"q1": ANIMATIONS[RUN_DIAGONAL_Q3],
+		"q2": ANIMATIONS[ATTACK_FRONT],
+	},
+	Vector2(1, 1): {
+		"q0": ANIMATIONS[IDLE_FRONT],
+		"q1": ANIMATIONS[RUN_DIAGONAL_Q4],
+		"q2": ANIMATIONS[ATTACK_FRONT],
+	}
+}
+
+func _ready():
+	last_direction = Vector2.ZERO
+	_update_animation(Vector2.ZERO)
 
 func _physics_process(_delta):
 	_move()
@@ -112,22 +159,35 @@ func _physics_process(_delta):
 func intance_particle():
 	var particle = PARTICLES.instance()
 	get_tree().root.add_child(particle)
-	particle.global_position = global_position + Vector2(0,16)
+	particle.global_position = global_position + Vector2(0, 16)
 	particle.z_index = 1
 	particle.play_particles()
 
 func _update_animation(direction):
-	if(direction != Vector2.ZERO and last_direction != direction):
+	is_running = direction != Vector2.ZERO
+
+	if is_running and last_direction != direction:
 		last_direction = direction
 	
-	animation_player.play(animation[directions[direction]]["name"])
-	sprite.set_flip_h(animation[directions[direction]]["flip"])
+	if is_running and not is_attacking:
+		current_state = DIRECTIONS[direction]['q1']
+	elif is_attacking and not is_running:
+		current_state = DIRECTIONS[last_direction]['q2']
+	elif not is_attacking and not is_running:
+		current_state = DIRECTIONS[last_direction]['q0']
+	
+	animation_player.play(current_state["anim_name"])
+	sprite.set_flip_h(current_state["flip"])
 
 func _move():
 	var direction = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	).normalized()
+	
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+
+	print(is_attacking)
 
 	_update_animation(direction.snapped(Vector2.ONE))
 
@@ -135,3 +195,6 @@ func _move():
 
 	velocity = move_and_slide(velocity, Vector2.ZERO)
 
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name.begins_with("attack_"):
+		is_attacking = false
